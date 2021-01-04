25dab08277d2f3920eed66f87b62404f926fac61
Ed Jefferson <e.jefferson@gmail.com>
2021-01-04 18:04:23 +0000

    styling, countries

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: redirect('/UK')
-  get 'vote', to: 'pages#vote'
+  get '/vote', to: 'pages#vote'
   get ':cc', to: 'pages#index', as: 'government'
 end
