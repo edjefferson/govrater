9819af5978c8f5bbb703498bbc4fcc1f258ddc41
Ed Jefferson <e.jefferson@gmail.com>
2021-01-08 18:39:47 +0000

    captcha wired

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -99,7 +99,7 @@ let votesLastSentTime = Date.now()
 let unsentVotes = 0
 let lastVoteRating = 0
 
-
+let clickMessages
 
 function sendUnsentVotes(refresh) {
   let country_code = $("#vote1").attr("data-govid")
@@ -260,11 +260,12 @@ function updateHistory(){
 }
 
 $('document').ready(function(){
+  
+  //$("#popup > button").click(function(){
+    
+  //})
 
-  $("#popup > button").click(function(){
-    $(".voteButton").attr("data-disabled","false")
-    $("#popup").hide()
-  })
+  
   achievementNumbers = [10,20,50,100,200,500,1000,2000,5000,10000,20000,50000,100000,200000,500000,1000000]
   cAchievementNumbers = [2,5,10,20,50,100,200,249]
   updateAchievements()
